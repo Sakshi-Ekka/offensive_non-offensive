6bf7a0400aaab462fc5f394170770b2f89ffe0a7
Sakshie <105942567+Sakshi-Ekka@users.noreply.github.com>
2024-06-30 15:24:26 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,55 @@
+import cv2
+import mediapipe as mp
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+from sklearn.metrics import accuracy_score                  # Accuracy metrics
+import pickle
+
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+df = pd.read_csv('data.csv')
+
+# print(df.head())
+# print(df.tail())
+
+X = df.drop('class', axis=1) # features                                     #axis=1 -> dropping class column
+y = df['class'] # target value                                              # taking only class column
+
+# print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
+
+pipelines = {
+    'lr': make_pipeline(StandardScaler(), LogisticRegression(max_iter=500)),
+    'rc': make_pipeline(StandardScaler(), RidgeClassifier()),
+    'rf': make_pipeline(StandardScaler(), RandomForestClassifier()),
+    'gb': make_pipeline(StandardScaler(), GradientBoostingClassifier()),
+    'SVM': make_pipeline(StandardScaler(), SVC()),
+    'MLP': make_pipeline(StandardScaler(), MLPClassifier(max_iter=500)),
+    'DT': make_pipeline(StandardScaler(), DecisionTreeClassifier()),
+    'AB': make_pipeline(StandardScaler(), AdaBoostClassifier())}
+
+fit_models = {}
+for algo, pipeline in pipelines.items():
+    model = pipeline.fit(X_train, y_train)
+    fit_models[algo] = model
+
+for algo, model in fit_models.items():
+    y_pred = model.predict(X_test)
+    print(algo, accuracy_score(y_test, y_pred))
+
+with open('model.pkl', 'wb') as f:
+    pickle.dump(fit_models['lr'], f)
